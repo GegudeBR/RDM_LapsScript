29bb0f97c35a24916104c3e7802d17958013f082
George <67044516+GegudeBR@users.noreply.github.com>
2022-07-20 23:13:40 -0700

    Updating structure

--- LAPS_Server.ps1
+++ LAPS_Server.ps1
@@ -1,36 +1,61 @@
+# Import
 param (
     [switch]$Unregister
 );
 Import-Module AdmPwd.PS
 
-$FileWatcher = New-Object System.IO.FileSystemWatcher
-$FileWatcher.Path = "L:\LAPS"
-$FileWatcher.Filter = "in.laps"
-$FileWatcher.EnableRaisingEvents = $true
+# UI Change
+$Title = "RDM LAPS Server"
+$host.UI.RawUI.WindowTitle = $Title
 
-$Fetching = $false
+$WindowSize = $Host.UI.RawUI.WindowSize
+$WindowSize.Width  = [Math]::Min(50, $Host.UI.RawUI.BufferSize.Width)
+$WindowSize.Height = 10
 
+try{
+  $Host.UI.RawUI.WindowSize = $WindowSize
+}
+catch [System.Management.Automation.SetValueInvocationException] {
+  $Maxvalue = ($_.Exception.Message |Select-String "\d+").Matches[0].Value
+  $WindowSize.Height = $Maxvalue
+  $Host.UI.RawUI.WindowSize = $WindowSize
+}
+
+
+# File Watcher
+$Folder = "L:\LAPS"
+$LogPath = $Folder + "\laps.log"
+$OutPath = $Folder + "\out.laps"
+$Filter = "in.laps"
+$FileWatcher = New-Object System.IO.FileSystemWatcher $Folder, $Filter -Property @{
+  EnableRaisingEvents = $true
+}
+
+# File Watcher Event Handlers
 $Action = {
-    if($Fetching) {
-        Return
-    }
-    $Fetching = $true
+  $FileWatcher.EnableRaisingEvents = $false
 	$Path = $Event.SourceEventArgs.FullPath 
-    Start-Sleep 0.3
+  Start-Sleep -Milliseconds 100
 	$ComputerName = Get-Content $Path
-    Write-Host "$(Get-Date): Request for $ComputerName"
+  Add-Content -Path $LogPath -Value "$(Get-Date): Request for $ComputerName"
 	$Password = Get-AdmPwdPassword -ComputerName $ComputerName | select -ExpandProperty Password
-	Set-Content -Path L:\LAPS\out.laps -Value $Password
-    $Fetching = $false
-    }  
+	Set-Content -Path $OutPath -Value $Password
+  $FileWatcher.EnableRaisingEvents = $true
+  }  
 
-if($Unregister) {
+
+
+if ($Unregister) {
   try {
     Get-EventSubscriber -SourceIdentifier LapsPS | Unregister-Event
   } catch {
     Write-Host Event not registered.
   }
-} else { 
+}
+else { 
   $Changed = Register-ObjectEvent $FileWatcher "Changed" -Action $Action -SourceIdentifier LapsPS
 }
+  
+
+
 
